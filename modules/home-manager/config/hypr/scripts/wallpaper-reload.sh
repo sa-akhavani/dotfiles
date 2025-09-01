8db0c49307f411b65146327e8e7a5bcf33a488c8
Ali <sa.akhavani@gmail.com>
2025-09-01 16:13:14 -0400

    update hyprland

---
+++ modules/home-manager/config/hypr/scripts/wallpaper-reload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+WALLPAPER_DIR="$HOME/Pictures/Wallpapers/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+# Get the name of the focused monitor with hyprctl
+FOCUSED_MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
+
+# Get a random wallpaper that is not the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
