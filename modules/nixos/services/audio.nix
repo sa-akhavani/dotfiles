a69953bfb685e35d64f3cfe88949011caf71f75a
Ali <sa.akhavani@gmail.com>
2025-09-02 11:20:10 -0400

    update pulseaudio

--- modules/nixos/services/audio.nix
+++ modules/nixos/services/audio.nix
@@ -2,20 +2,20 @@
 
 {
   # sound.enable = true;
-  # security.rtkit.enable = true;
   # hardware.pulseaudio.enable = false;
 
+  security.rtkit.enable = true;		# (optional, recommended) allows Pipewire to use the realtime scheduler for increased performance.
   services.pipewire = {
 		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
 		pulse.enable = true;
-#		alsa.enable = true;
-#		alsa.support32Bit = true;
-#		jack.enable = true;
+		jack.enable = true;
 #		wireplumber.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
-    pamixer
-    pavucontrol
+    pamixer				# Pulseaudio command line mixer
+    pavucontrol				# PulseAudio Volume Control
   ];
 }
