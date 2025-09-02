a2e11bc539d6c589139b835b6f3dca46ebe0c68a
Ali <sa.akhavani@gmail.com>
2025-09-02 17:39:35 -0400

    wip nvim config

--- modules/home-manager/config/nvim/lua/ali/plugins/telescope.lua
+++ modules/home-manager/config/nvim/lua/ali/plugins/telescope.lua
@@ -5,11 +5,11 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build =
-      "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-    },
+    -- {
+    --   "nvim-telescope/telescope-fzf-native.nvim",
+    --   build =
+    --   "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    -- },
     "nvim-tree/nvim-web-devicons",
     "BurntSushi/ripgrep",
     "sharkdp/fd",
@@ -52,17 +52,17 @@ return {
         },
       },
       extensions = {
-        fzf = {
-          fuzzy = true,              -- false will only do exact matching
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = "smart_case",  -- or "ignore_case" or "respect_case"
-          -- the default case_mode is "smart_case"
-        },
+        -- fzf = {
+        --   fuzzy = true,              -- false will only do exact matching
+        --   override_generic_sorter = true, -- override the generic sorter
+        --   override_file_sorter = true, -- override the file sorter
+        --   case_mode = "smart_case",  -- or "ignore_case" or "respect_case"
+        --   -- the default case_mode is "smart_case"
+        -- },
       },
     })
 
-    telescope.load_extension("fzf")
+    -- telescope.load_extension("fzf")
     -- telescope.load_extension("fd")
 
     -- set keymaps
