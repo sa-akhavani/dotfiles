e02c58a819c74d39f04d2c927115bff48299d24a
Ali <sa.akhavani@gmail.com>
2025-09-03 15:42:49 -0400

    update nvim lsp

---
+++ modules/home-manager/config/nvim/lua/ali/plugins/lsp/conform.lua
@@ -0,0 +1,23 @@
+-- Lightweight yet powerful formatter plugin for Neovim
+return {
+  'stevearc/conform.nvim',
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      c = { "clang_format", lsp_format = "fallback" },
+      cpp = { "clang_format", lsp_format = "fallback" },
+      json = { "prettier" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      python = { "ruff", lsp_format = "fallback" },
+      markdown = { "prettier", lsp_format = "fallback" },
+      nix = { "nixfmt" },
+    },
+    format_on_save = {
+      timeout_ms = 2000,
+      lsp_format = "fallback",
+    },
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {});
+  end
+}
--- modules/home-manager/config/nvim/lua/ali/plugins/lsp/lsp-config.lua
+++ modules/home-manager/config/nvim/lua/ali/plugins/lsp/lsp-config.lua
@@ -13,6 +13,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
+	  "ruff", --python
           "tsserver",
         }
       })
@@ -26,6 +27,7 @@ return {
 
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.ruff.setup({})
       lspconfig.tsserver.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
