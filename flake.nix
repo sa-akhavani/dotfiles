8dc5e8d7be3a5c351827c886dee8fa16cdcf4dcb
Ali <sa.akhavani@gmail.com>
2025-10-02 16:45:59 -0400

    reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -9,59 +9,65 @@
     # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
-#     url = "github:nix-community/home-manager";
+      #     url = "github:nix-community/home-manager";
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      ...
+    }@inputs:
     let
- 	system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-	pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
     in
     {
-	    # NixOS configuration entrypoint
-	    # Available through 'nixos-rebuild --flake .#your-hostname'
-	    nixosConfigurations = {
-		
-		rostam = nixpkgs.lib.nixosSystem {
-		      inherit system;
-		      specialArgs = {
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+
+        rostam = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
             inherit inputs;
           };
-		      modules = [
-			./hosts/rostam/configuration.nix
+          modules = [
+            ./hosts/rostam/configuration.nix
 
-			home-manager.nixosModules.home-manager
-			{
-				home-manager.useGlobalPkgs = true;
-				home-manager.useUserPackages = true;
-				home-manager.users.ali = import ./hosts/rostam/home.nix;
-				home-manager.extraSpecialArgs = { 
-					inherit inputs; 
-					inherit pkgs-unstable;
-				};
-			}
-		      ];
-		};
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.ali = import ./hosts/rostam/home.nix;
+              home-manager.extraSpecialArgs = {
+                inherit inputs;
+                inherit pkgs-unstable;
+              };
+            }
+          ];
+        };
 
-	     };
+      };
 
-
-	     # Standalone home-manager configuration entrypoint
-	     # Available through 'home-manager --flake .#your-username@your-hostname'
-#	     homeConfigurations = {
-#		ali = home-manager.lib.homeManagerConfiguration {
-#			inherit pkgs;
-#			modules = [ ./home ];
-#			extraSpecialArgs = {
-#			    inherit pkgs;
-#			    inherit inputs;
-#			};
-#		};
-#	    };
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      #	     homeConfigurations = {
+      #		ali = home-manager.lib.homeManagerConfiguration {
+      #			inherit pkgs;
+      #			modules = [ ./home ];
+      #			extraSpecialArgs = {
+      #			    inherit pkgs;
+      #			    inherit inputs;
+      #			};
+      #		};
+      #	    };
 
     };
 }
