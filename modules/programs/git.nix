0d6df4e6ceeff8aa647854c0e92346cbe2d543a2
Ali <sa.akhavani@gmail.com>
2026-02-11 16:21:49 -0500

    fix hyprland config

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -7,11 +7,7 @@
       user.name = "Ali";
       user.email = "sa.akhavani@gmail.com";
     };
-    alias = {
-      ci = "commit";
-      co = "checkout";
-      s = "status";
-    };
+
     delta.enable = true;
     delta.options = {
       features = "dracula";
