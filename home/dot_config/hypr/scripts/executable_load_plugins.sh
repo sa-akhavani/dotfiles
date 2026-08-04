530a449722226a5f98b5e66270dcd0d34aa2ea27
Ali <sa.akhavani@gmail.com>
2026-08-04 17:06:42 -0400

    fix hyprland and plugin errors

--- home/dot_config/hypr/scripts/executable_load_plugins.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sleep 0.8 # Give Hyprland a moment to fully start
-# On Arch, hyprsplit is installed/enabled via hyprpm (see install.sh).
-# hyprpm reload loads all currently-enabled plugins.
-hyprpm reload -n
